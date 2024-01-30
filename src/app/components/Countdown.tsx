de554a0f8fa1324121727d73171ffe922719e172
Vishnu <vpgit123@gmail.com>
2024-01-30 18:41:10 +1100

    countdown completed

---
+++ src/app/components/Countdown.tsx
@@ -0,0 +1,53 @@
+"use client"
+import React, { useEffect, useState } from "react";
+
+const Countdown: React.FC = () => {
+
+  const targetDate = "2024-02-17T10:00:00+1100";
+
+  const calculateTimeLeft = () => {
+    const difference = new Date(targetDate).getTime() - new Date().getTime();
+    if (difference <= 0) {
+      // Target date has passed
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    }
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    return { days, hours, minutes, seconds };
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [targetDate]);
+
+  const formatTime = (value: number) => value.toString().padStart(2, '0');
+
+  return (
+    <div className="z-10 flex flex-col p-4 sm:ml-5  md:ml-10 bg-black w-3/5 justify-center items-center rounded-2xl bg-opacity-50">
+      <span className="text-white text-3xl font-bold" >Next MegaLan -&quot;Might and Magic&quot; Begins in:</span>
+
+      
+      <div className="text-4xl sm:text-6xl w-fit mt-5 font-extrabold text-white">
+      <div>{formatTime(timeLeft.days)} D : {formatTime(timeLeft.hours)} H : {formatTime(timeLeft.minutes)} M : {formatTime(timeLeft.seconds)} S</div>
+      </div>
+    
+    </div>
+  );
+
+  return (<></>);
+
+}
+
+export default Countdown;
--- src/app/screens/LandingScreen.tsx
+++ src/app/screens/LandingScreen.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Countdown from "../components/Countdown";
 const LandingScreen: React.FC = () => {
   return (
     <>
@@ -11,19 +12,20 @@ const LandingScreen: React.FC = () => {
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
-        className="h-full bg-white opacity-75 "
+        className="h-full bg-white opacity-80 "
       >
         <div className="h-full w-full flex flex-col ">
-          <div className="h-full">
-          <div className=" w-full md:w-1/2 p-4 sm:mt-5 sm:ml-5 md:mt-10 md:ml-10  ">
-            <span className="text-5xl sm:text-7xl md:text-9xl text-wrap  text-white font-extrabold">
-              UNSW&apos;S GAMING CONVENTION
-            </span>
+          <div className="h-full ">
+            <div className=" w-full md:w-1/2 p-4 sm:mt-5 sm:ml-5 md:mt-10 md:ml-10  ">
+              <span className="text-5xl sm:text-7xl md:text-9xl text-wrap  text-white font-extrabold">
+                UNSW&apos;S GAMING CONVENTION
+              </span>
+            </div>
+            <Countdown />
           </div>
+          <div className="animate-bounce text-2xl md:text-4xl justify-end  bottom-0">
+            <div className="w-full text-center text-black">&#8595;</div>
           </div>
-        <div className="animate-bounce text-2xl md:text-4xl justify-end  bottom-0">
-          <div className="w-full text-center text-black">&#8595;</div>
-        </div>
         </div>
       </div>
     </>
