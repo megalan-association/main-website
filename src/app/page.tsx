55694a044458d1ce234792c8784a0962ff50df97
Vishnu <vpgit123@gmail.com>
2024-02-02 17:21:35 +1100

    added strawpoll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,30 @@ export default function Home() {
       <div className="min-h-screen w-full">
         <LandingScreen />
       </div>
+
+      <div
+        className="h-screen w-full flex flex-col"
+        id="strawpoll_bVg8oMG13nY"
+        // style="height: 1028px; max-width: 640px; width: 100%; margin: 0 auto; display: flex; flex-direction: column;"
+      >
+        <iframe
+          title="StrawPoll Embed"
+          id="strawpoll_iframe_bVg8oMG13nY"
+          className="static visible block w-full h-screen "
+          src="https://strawpoll.com/embed/bVg8oMG13nY"
+          // style="position: static; visibility: visible; display: block; width: 100%; flex-grow: 1;"
+          // frameborder="0"
+          // allowfullscreen
+          // allowtransparency
+        >
+          Loading...
+        </iframe>
+        <script
+          async
+          src="https://cdn.strawpoll.com/dist/widgets.js"
+          // charset="utf-8"
+        ></script>
+      </div>
       <div className="flex flex-row min-h-screen justify-center bg-neutral-700">
         <div className="w-full md:container ">
           <SocGrid />
